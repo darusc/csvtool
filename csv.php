a3f2bbecb3ead61499312125caf9f35898ea616c
Darius <dariuscirneala05@gmail.com>
2025-07-22 21:05:42 +0300

    Added positional & optional arguments + added help menu

--- csv.php
+++ csv.php
@@ -4,10 +4,33 @@ require __DIR__ . '/vendor/autoload.php';
 
 use \Csvtool\Application;
 
-try {
-    $app = Application::create($argc, $argv);
-    $app->run();
-} catch (InvalidArgumentException $e) {
-    print "Error: " . $e->getMessage() . PHP_EOL;
-    print "Usage: php csvtool.php <action> [options]\n";
+if ($argv[1] == '--help') {
+    print_help(Application::$actionMap);
+} else {
+    try {
+        $app = Application::create($argc, $argv);
+        $app->run();
+    } catch (InvalidArgumentException $e) {
+        print "Error: " . $e->getMessage() . PHP_EOL;
+        print "Usage: php csv.php <action> [options]" . PHP_EOL;
+    }
+}
+
+function print_help(array $actions): void
+{
+    print "Usage: php csv.php <action> <args...> [options...]" . PHP_EOL . PHP_EOL;
+    foreach ($actions as $action) {
+        $command = $action::getDefinition();
+
+        print $command['name'] . ' - ' . $command['description'] . PHP_EOL;
+        print '    php csv.php' . $command['name'];
+        foreach ($command['args'] as $arg) {
+            if (str_starts_with($arg, '--')) {
+                print ' [' . $arg . ']';
+            } else {
+                print ' ' . $arg;
+            }
+        }
+        print PHP_EOL;
+    }
 }
